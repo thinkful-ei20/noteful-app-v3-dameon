f27244f69acfc788a68bfa45100a24afbdc3e275
Chris Klanac <cklanac@users.noreply.github.com>
2018-02-13 07:58:08 -0600

    comment out api calls

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -10,5 +10,17 @@ $(document).ready(function () {
       noteful.render();
     });
 
+  // api.search('/v3/folders')
+  //   .then(response => {
+  //     store.folders = response;
+  //     noteful.render();
+  //   });
+
+  // api.search('/v3/tags')
+  //   .then(response => {
+  //     store.tags = response;
+  //     noteful.render();
+  //   });
+
 });
 
