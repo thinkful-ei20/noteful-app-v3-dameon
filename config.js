8ad2a62799fbd1f92c997d9df85fc8e34c1a914c
Dameon1 <dameonmedoza@gmail.com>
2018-05-02 12:11:04 -0700

    testF

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ exports.PORT = process.env.PORT || 8080;
 exports.MONGODB_URI = process.env.MONGODB_URI ||  'mongodb://dev:dev@ds111050.mlab.com:11050/noteful-app';
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://dev:dev@ds111050.mlab.com:11050/noteful-app';
 //extra
-//'mongodb://localhost/noteful-app' ||  
+//'mongodb://localhost/noteful-app' ||   
